d6b33315ead92578cb41bbe37b2e0cbd05a80daf
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2024-09-23 09:34:20 +0700

    more extension on context

--- lib/extension.dart
+++ lib/extension.dart
@@ -3,7 +3,15 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 extension on BuildContext {
   ThemeData get theme => Theme.of(this);
+  MediaQueryData get mediaQuery => MediaQuery.of(this);
+  NavigatorState get navigator => Navigator.of(this);
+  Size get screenSize => mediaQuery.size;
+  double get screenWidth => screenSize.width;
+  double get screenHeight => screenSize.height;
+  Orientation get orientation => mediaQuery.orientation;
   ColorScheme get colorScheme => theme.colorScheme;
-  T getBloc<T extends StateStreamableSource<Object?>>({bool listen = false}) => BlocProvider.of<T>(this, listen: listen);
-  T getRepo<T extends Object>({bool listen = false}) => RepositoryProvider.of<T>(this, listen: listen);
+  T getBloc<T extends StateStreamableSource<Object?>>({bool listen = false}) =>
+      BlocProvider.of<T>(this, listen: listen);
+  T getRepo<T extends Object>({bool listen = false}) =>
+      RepositoryProvider.of<T>(this, listen: listen);
 }
