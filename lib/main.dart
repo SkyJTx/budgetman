8a99b566cdeef834854f58b3ff4e3c101f589970
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2024-10-02 23:48:17 +0700

    test if id works

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:budgetman/client/bloc/settings/settings_bloc.dart';
+import 'package:budgetman/client/component/theme.dart';
+import 'package:budgetman/server/data_model/setting.dart';
 import 'package:budgetman/server/repository/settings/settings_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:budgetman/dependencies_injector.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:isar/isar.dart';
 import 'package:sizer/sizer.dart';
 
 void main() async {
@@ -51,6 +54,7 @@ class BudgetManAppState extends State<BudgetManApp> {
 
   @override
   Widget build(BuildContext context) {
+    final baseThemes = MaterialTheme(const TextTheme().apply());
     return ResponsiveSizer(
       builder: (context, orientation, deviceType) {
         return BlocConsumer<SettingsBloc, SettingsState>(
@@ -61,25 +65,17 @@ class BudgetManAppState extends State<BudgetManApp> {
             return MaterialApp(
               debugShowCheckedModeBanner: false,
               title: 'BudgetMan',
-              theme: ThemeData.from(
-                colorScheme: ColorScheme.fromSeed(
-                  seedColor: Colors.yellow,
-                  brightness: Brightness.light,
-                ),
-              ),
-              darkTheme: ThemeData.from(
-                colorScheme: ColorScheme.fromSeed(
-                  seedColor: Colors.yellow,
-                  brightness: Brightness.dark,
-                ),
-              ),
+              theme: baseThemes.light(),
+              darkTheme: baseThemes.dark(),
+              highContrastTheme: baseThemes.lightHighContrast(),
+              highContrastDarkTheme: baseThemes.darkHighContrast(),
               themeMode: state.theme,
               home: Scaffold(
                 appBar: AppBar(
                   title: const Text('BudgetMan'),
                 ),
                 body: Center(
-                  child: Column(
+                  child: ListView(
                     children: [
                       Text('Hello, ${state.name}'),
                       ListTile(
@@ -95,20 +91,70 @@ class BudgetManAppState extends State<BudgetManApp> {
                       ),
                       ListTile(
                         title: const Text('Theme'),
-                        trailing: Builder(builder: (context) {
-                          bool isDark = state.theme == ThemeMode.dark;
-                          if (state.theme == ThemeMode.system) {
-                            isDark =
-                                WidgetsBinding.instance.platformDispatcher.platformBrightness ==
-                                    Brightness.dark;
+                        trailing: Builder(
+                          builder: (context) {
+                            bool isDark = state.theme == ThemeMode.dark;
+                            if (state.theme == ThemeMode.system) {
+                              isDark =
+                                  WidgetsBinding.instance.platformDispatcher.platformBrightness ==
+                                      Brightness.dark;
+                            }
+                            return Switch.adaptive(
+                              value: isDark,
+                              onChanged: (value) {
+                                context.read<SettingsBloc>().toggleTheme();
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                      FutureBuilder(
+                        future: () async {
+                          final isar = Isar.getInstance();
+                          if (isar == null) {
+                            throw Exception('Isar instance is not initialized.');
                           }
-                          return Switch.adaptive(
-                            value: isDark,
-                            onChanged: (value) {
-                              context.read<SettingsBloc>().toggleTheme();
-                            },
-                          );
-                        }),
+                          final setting = isar.settings;
+                          final name = await setting.getByKey('name');
+                          if (name == null) {
+                            throw Exception('Name setting is not initialized.');
+                          }
+                          return name;
+                        }(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState == ConnectionState.waiting) {
+                            return const CircularProgressIndicator();
+                          }
+                          if (snapshot.hasError) {
+                            return Text('Error: ${snapshot.error}');
+                          }
+                          final name = snapshot.data as Setting;
+                          return Text('Hello, ${name.value} ${name.id}');
+                        },
+                      ),
+                      FutureBuilder(
+                        future: () async {
+                          final isar = Isar.getInstance();
+                          if (isar == null) {
+                            throw Exception('Isar instance is not initialized.');
+                          }
+                          final setting = isar.settings;
+                          final theme = await setting.getByKey('theme');
+                          if (theme == null) {
+                            throw Exception('theme setting is not initialized.');
+                          }
+                          return theme;
+                        }(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState == ConnectionState.waiting) {
+                            return const CircularProgressIndicator();
+                          }
+                          if (snapshot.hasError) {
+                            return Text('Error: ${snapshot.error}');
+                          }
+                          final theme = snapshot.data as Setting;
+                          return Text('Hello, ${theme.value} ${theme.id}');
+                        },
                       ),
                     ],
                   ),
