678e15f90056ec76cdf31911b4b0e3529a5fad96
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2024-10-07 15:57:34 +0700

    add app bar

--- lib/client/presentation/main_page.dart
+++ lib/client/presentation/main_page.dart
@@ -1,12 +1,15 @@
+import 'package:budgetman/client/bloc/settings/settings_bloc.dart';
+import 'package:budgetman/extension.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:sizer/sizer.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({
     super.key,
-     required this.state,
+    required this.state,
     required this.child,
-   
   });
 
   final GoRouterState state;
@@ -19,8 +22,25 @@ class MainPage extends StatefulWidget {
 class MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: widget.child,
+    return BlocBuilder<SettingsBloc, SettingsState>(
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: ListTile(
+              title: const Text('BudgetMan App'),
+              subtitle: Text(widget.state.matchedLocation),
+              titleTextStyle: context.textTheme.titleLarge,
+            ),
+          ),
+          drawer: const Drawer(),
+          body: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all([3.w, 3.h].min.toDouble()),
+              child: widget.child,
+            ),
+          ),
+        );
+      },
     );
   }
 }
