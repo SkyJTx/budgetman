66efcef8e14c76e1a8baa1cd9092da8bf0c39136
KamitomeYuta <142527511+KamitomeYuta@users.noreply.github.com>
2024-10-13 14:29:31 +0700

    add drawer and options button

--- lib/client/presentation/main_page.dart
+++ lib/client/presentation/main_page.dart
@@ -1,4 +1,8 @@
 import 'package:budgetman/client/bloc/settings/settings_bloc.dart';
+import 'package:budgetman/client/component/options_button.dart';
+import 'package:budgetman/client/presentation/categories/categories_page.dart';
+import 'package:budgetman/client/presentation/home/home_page.dart';
+import 'package:budgetman/client/presentation/setting/setting_page.dart';
 import 'package:budgetman/extension.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -20,11 +24,14 @@ class MainPage extends StatefulWidget {
 }
 
 class MainPageState extends State<MainPage> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SettingsBloc, SettingsState>(
       builder: (context, state) {
         return Scaffold(
+          key: scaffoldKey,
           appBar: AppBar(
             title: ListTile(
               title: const Text('BudgetMan App'),
@@ -32,13 +39,74 @@ class MainPageState extends State<MainPage> {
               titleTextStyle: context.textTheme.titleLarge,
             ),
           ),
-          drawer: const Drawer(),
+          drawer: Drawer(
+            child: ListView(
+              children: [
+                Align(
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 8.h,
+                          child: Icon(
+                            Icons.account_circle,
+                            size: 8.h,
+                          ),
+                        ),
+                        const Text('BudgetMan App',
+                            style: TextStyle(fontSize: 30)),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 2.h),
+                ListTile(
+                  leading: const Icon(Icons.home),
+                  title: const Text('Home'),
+                  onTap: () {
+                    context.go(HomePage.routeName);
+                    scaffoldKey.currentState?.closeDrawer();
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.list),
+                  title: const Text('Budget Manager'),
+                  onTap: () {
+                    context.go('/budget');
+                    scaffoldKey.currentState?.closeDrawer();
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.category),
+                  title: const Text('Categories Manager'),
+                  onTap: () {
+                    context.go(CategoriesPage.routeName);
+                    scaffoldKey.currentState?.closeDrawer();
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.settings),
+                  title: const Text('Settings'),
+                  onTap: () {
+                    context.go(SettingPage.routeName);
+                    scaffoldKey.currentState?.closeDrawer();
+                  },
+                ),
+              ],
+            ),
+          ),
           body: SafeArea(
             child: Padding(
               padding: EdgeInsets.all([3.w, 3.h].min.toDouble()),
               child: widget.child,
             ),
           ),
+          floatingActionButton: widget.state.matchedLocation == '/setting'
+              ? null
+              : OptionsButton(
+                  locate: widget.state.matchedLocation,
+                ),
         );
       },
     );
