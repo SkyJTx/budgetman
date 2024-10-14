ff9788faf807cf6695a5df587777fbe5b88879c8
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2024-10-14 22:49:57 +0700

    Refactor budget list edit page to add initial implementation

---
+++ lib/client/presentation/budget/budget_list/edit_budget_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditBudgetList extends StatefulWidget {
+  const EditBudgetList({super.key});
+
+  @override
+  State<EditBudgetList> createState() => EditBudgetListState();
+}
+
+class EditBudgetListState extends State<EditBudgetList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
