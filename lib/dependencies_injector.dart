6a90ee0074b450fd78222b007b4e95cc2f4b28a9
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2024-10-17 16:08:05 +0700

    restrict app platform

--- lib/dependencies_injector.dart
+++ lib/dependencies_injector.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:math';
 
 import 'package:budgetman/server/data_model/budget.dart';
@@ -13,49 +14,55 @@ import 'package:get_it/get_it.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 
-Future<void> init() async {
-  WidgetsFlutterBinding.ensureInitialized();
+Future<bool> init() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
 
-  final getit = GetIt.instance;
+    final getit = GetIt.instance;
 
-  // Initialize the database
-  final dir = await getApplicationDocumentsDirectory();
-  final isar = await Isar.open(
-    [BudgetSchema, BudgetListSchema, CategorySchema, SettingSchema],
-    directory: dir.path,
-    inspector: kDebugMode,
-  );
-
-  if (kDebugMode) {
-    await isar.writeTxn(() async {
-      await Future.wait([
-        isar.budgets.clear(),
-        isar.budgetLists.clear(),
-        isar.categorys.clear(),
-      ]);
-    });
-    var budget = await BudgetRepository().add(
-      name: 'Budget 1',
-      description: 'Budget 1 Description',
-      startDate: DateTime.now(),
-      endDate: DateTime.now().add(30.days),
-      isRoutine: true,
-      routineInterval: 10.days.inSeconds,
-      isCompleted: false,
-      isRemoved: false,
+    // Initialize the database
+    final dir = await getApplicationDocumentsDirectory();
+    final isar = await Isar.open(
+      [BudgetSchema, BudgetListSchema, CategorySchema, SettingSchema],
+      directory: dir.path,
+      inspector: kDebugMode,
     );
-    for (int i = 1; i <= 30; i++) {
-      await BudgetListRepository().add(
-        budget,
-        title: 'Budget List $i',
-        description: 'Budget List $i Description',
-        priority: 1,
-        amount: Random().nextDouble() * 100000,
-        deadline: DateTime.now().add(i.days),
-        isCompleted: i % 2 == 0,
-      );
-    }
-  }
 
-  getit.registerSingleton<Isar>(isar, dispose: (isar) => isar.close());
+    if (kDebugMode) {
+      await isar.writeTxn(() async {
+        await Future.wait([
+          isar.budgets.clear(),
+          isar.budgetLists.clear(),
+          isar.categorys.clear(),
+        ]);
+      });
+      var budget = await BudgetRepository().add(
+        name: 'Budget 1',
+        description: 'Budget 1 Description',
+        startDate: DateTime.now(),
+        endDate: DateTime.now().add(30.days),
+        isRoutine: true,
+        routineInterval: 10.days.inSeconds,
+        isCompleted: false,
+        isRemoved: false,
+      );
+      for (int i = 1; i <= 30; i++) {
+        await BudgetListRepository().add(
+          budget,
+          title: 'Budget List $i',
+          description: 'Budget List $i Description',
+          priority: 1,
+          amount: Random().nextDouble() * 100000,
+          deadline: DateTime.now().add(i.days),
+          isCompleted: i % 2 == 0,
+        );
+      }
+    }
+
+    getit.registerSingleton<Isar>(isar, dispose: (isar) => isar.close());
+
+    return Platform.isAndroid || Platform.isIOS;
+  } catch (e, _) {
+    return false;
+  }
 }
