d1d87d2413b7c27dc294e6bf8215703db4fa9b6f
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2024-10-17 21:07:30 +0700

    use push instead of showDialog

--- lib/client/component/loading_overlay.dart
+++ lib/client/component/loading_overlay.dart
@@ -1,33 +1,47 @@
 import 'dart:async';
 
-import 'package:budgetman/client/repository/global_repo.dart';
 import 'package:flutter/material.dart';
 
-class LoadingOverlay extends Dialog {
-  LoadingOverlay({super.key})
-      : super(
-          backgroundColor: Colors.black.withOpacity(0.5),
+class LoadingOverlay extends StatelessWidget {
+  final double size;
+
+  const LoadingOverlay({
+    super.key,
+    this.size = 50,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black.withOpacity(0.5),
+      body: Center(
+        child: Container(
+          width: size,
+          height: size,
           alignment: Alignment.center,
-          child: const PopScope(
-            canPop: false,
-            child: CircularProgressIndicator(
-              color: Colors.white,
-            ),
+          child: CircularProgressIndicator(
+            color: Colors.white,
+            strokeWidth: size / 12.5,
           ),
-        );
+        ),
+      ),
+    );
+  }
 
   static void show(BuildContext context) {
-    showDialog<void>(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) => LoadingOverlay(),
+    Navigator.of(context).push(
+      PageRouteBuilder(
+        opaque: false,
+        pageBuilder: (context, _, __) => const LoadingOverlay(),
+      ),
     );
   }
 
   static Future<T> wait<T>(BuildContext context, FutureOr<T> future) async {
+    final navigator = Navigator.of(context);
     show(context);
     final result = await future;
-    ClientRepository.navigatorKey.currentState?.pop();
+    navigator.pop();
     return result;
   }
 }
