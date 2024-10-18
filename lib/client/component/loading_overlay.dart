1dd9168aac047b307ab6f728fa37cba7670dc1e7
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2024-10-18 13:58:31 +0700

    add popup check

--- lib/client/component/loading_overlay.dart
+++ lib/client/component/loading_overlay.dart
@@ -1,26 +1,29 @@
 import 'dart:async';
 
+import 'package:budgetman/server/component/extension.dart';
 import 'package:flutter/material.dart';
 
 class LoadingOverlay extends StatelessWidget {
   final double size;
+  final bool isPopup;
 
   const LoadingOverlay({
     super.key,
     this.size = 50,
+    this.isPopup = false,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black.withOpacity(0.5),
+      backgroundColor: isPopup ? Colors.black.withOpacity(0.5) : context.theme.colorScheme.surface,
       body: Center(
         child: Container(
           width: size,
           height: size,
           alignment: Alignment.center,
           child: CircularProgressIndicator(
-            color: Colors.white,
+            color: isPopup ? Colors.white : context.theme.colorScheme.primary,
             strokeWidth: size / 12.5,
           ),
         ),
@@ -32,7 +35,9 @@ class LoadingOverlay extends StatelessWidget {
     Navigator.of(context).push(
       PageRouteBuilder(
         opaque: false,
-        pageBuilder: (context, _, __) => const LoadingOverlay(),
+        pageBuilder: (context, _, __) => const LoadingOverlay(
+          isPopup: true,
+        ),
       ),
     );
   }
