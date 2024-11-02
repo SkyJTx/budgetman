3568c121f16112081bc4d5c535278817a0e7cd4c
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2024-11-02 23:38:03 +0700

    add flutter local notification

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import Flutter
 import UIKit
 
+import flutter_local_notifications
+
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -9,6 +11,9 @@ import UIKit
   ) -> Bool {
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+      GeneratedPluginRegistrant.register(with: registry)
     }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
